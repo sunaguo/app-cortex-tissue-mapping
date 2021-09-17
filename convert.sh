b3025015b28d16838bb69a5d581faa90bacb2b2e
Brad Caron <bacaron@iu.edu>
2021-09-17 11:45:39 -0400

    reason: small optimizations

--- convert.sh
+++ convert.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # set top variables
-surf_data=`jq -r '.surf_data' config.json`
-surf_verts=`jq -r '.surf_verts' config.json`
-data_type=`jq -r '.type' config.json`
+surf_data_left=`jq -r '.surf_data_lh' config.json`
+surf_data_right=`jq -r '.surf_data_rh' config.json`
+surf_verts_left=`jq -r '.surf_verts_lh' config.json`
+surf_verts_right=`jq -r '.surf_verts_rh' config.json`
 label=`jq -r '.label' config.json`
 hemi="left right"
 
@@ -11,17 +12,18 @@ hemi="left right"
 # make cortexmap directories
 [ ! -d ./cortexmap ] && mkdir -p cortexmap cortexmap/cortexmap cortexmap/cortexmap/func cortexmap/cortexmap/surf cortexmap/cortexmap/label
 
-## vertices
 # loop through hemispheres, copy files that don't need conversion / convert files that do
 for h in ${hemi}
 do
+	## vertices
 	# set directory
-	tmpdir=${surf_verts}/${h}
+	$(eval "echo \$lmax${lmax}")
+	tmpdir=$(eval "echo \$surf_verts_${h}")
 
 	# grab data files
 	files=(`ls ${tmpdir}`)
 
-	# identify if files need to be converted
+	# identify if files need to be converted; if so, convert. if not, straight copy
 	for i in ${files}
 	do
 		if [[ ${i} == *".surf.gii"* ]]; then
@@ -31,33 +33,28 @@ do
 			cp ${tmpdir}/${i} ./cortexmap/cortexmap/surf/lh.${substr}.surf.gii
 		fi
 	done
-done
 
-## data
-tmpdir=${surf_data}
-if [ -f ${label} ]; then
-	importdir="label"
-	for h in ${hemi}
-	do
-		if [[ ${h} == 'left' ]]; then
-			hem="lh"
-		else
-			hem="rh"
-		fi
-		cp ${tmpdir}/${h}.gii cortexmap/cortexmap/label/${hem}.parc.label.gii
-	done
-else
-	importdir="func"
-	for h in ${hemi}
-	do
-		if [[ ${h} == 'left' ]]; then
-			hem="lh"
-		else
-			hem="rh"
-		fi
-		cp ${tmpdir}/${h}.gii cortexmap/cortexmap/func/${hem}.data.func.gii
-	done
-fi
+	## surface data
+	# set hemisphere substring for connectome workbench format
+	if [[ ${h} == 'left' ]]; then
+		hem="lh"
+	else
+		hem="rh"
+	fi
+
+	# if label.json exists, then the data is probably parcellation data. if not, it's probably func data. identify and set appropriate output dirs and names
+	if [ -f ${label} ]; then
+		importdir="label"
+		outname="parc.label.gii"
+	else
+		importdir="func"
+		outname="data.func.gii"
+	fi
+
+	# copy data
+	tmpdata=$(eval "echo \$surf_data_${h}")
+	cp ${tmpdata} ./cortexmap/cortexmap/${importdir}/${hem}.${outname}
+done
 
 # final check
 [ "$(ls -A ./cortexmap/cortexmap/${importdir})" ] && echo "something went wrong. check logs and derivatives" && exit 1 || echo "complete" && exit 0
